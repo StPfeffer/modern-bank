35ac13412096fa4814aa9316f12149fa0fa52ab4
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2022-12-04 15:31:19 -0300

    App.jsx template fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,28 @@ const App = () => (
     {/* Navbar */}
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        Navbar
+        <Navbar />
       </div>
     </div>
 
     {/* Hero */}
     <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Hero
+        <Hero />
       </div>
     </div>
 
     {/*  */}
     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
-        Stats
-        Business
-        Billing
-        CardDeal
-        Testimonials
-        Clients
-        CTA
-        Footer
+        <Stats />
+        <Business />
+        <Billing />
+        <CardDeal />
+        <Testimonials />
+        <Clients />
+        <CTA />
+        <Footer />
       </div>
     </div>
   </div>
