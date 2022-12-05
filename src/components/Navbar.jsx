6cfd2f9bf0ae3e86086024c996fd47e9f3fd8115
Mateus Pfeffer <92607757+StPfeffer@users.noreply.github.com>
2022-12-05 15:34:57 -0300

    navbar hover effect and logo link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,13 +10,15 @@ const Navbar = () => {
 
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
-      <img src={logo} alt='hoobank' className='w-[124px] h-[32px]' />
+      <a href="#">
+        <img src={logo} alt='hoobank' className='w-[124px] h-[32px]' />
+      </a>
 
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index) => (
           <li
             key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] ${active === nav.title ? 'text-white' : 'text-dimWhite'} ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'}`}
+            className={`font-poppins font-normal cursor-pointer text-[16px] ${active === nav.title ? 'text-white' : 'text-dimWhite'} ${index === navLinks.length - 1 ? 'mr-0' : 'mr-10'} hover:text-gray-300`}
             onClick={() => setActive(nav.title)} >
             <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
